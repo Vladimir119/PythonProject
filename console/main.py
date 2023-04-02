1f7a954f0cacd07fdf08ee0278e7273811cb3efa
Yurchenko vladimir <vova_119@bk.ru>
2023-04-02 16:11:31 +0300

    add save data in data.json

--- console/main.py
+++ console/main.py
@@ -6,11 +6,15 @@ import time
 end_of_input = ['\n', '\t', ' ']
 
 class Test():
-    def __init__(self, count_word_in_text=0, count_error=0, speed=0, time=0):
+    def __init__(self, count_word_in_text=0, count_error=0, speed=0, time_test=0):
         self.count_word_in_text = count_word_in_text
         self.count_error = count_error
         self.speed = speed
-        self.time = time
+        self.time_test = time_test
+
+    def __str__(self):
+        return str(self.count_word_in_text) + ' ' + str(self.count_error) + ' ' + str(self.speed) + ' ' + str(self.time_test)
+
     def getList(self):
         return [count_word_in_text, count_error, speed]
 
@@ -26,23 +30,25 @@ class DadaProcessor():
 
     @staticmethod
     def loadAllTest():
-        with open("data", 'r') as file:
+        with open("data.json", 'r') as file:
             all_data = json.load(file)
             all_test = all_data["data of tests"]
         return all_test
 
     @staticmethod
-    def uploudTests(test):
-
+    def uploudTests():
         count_of_test = DadaProcessor.uploadCountOfTest()
         all_test = DadaProcessor.loadAllTest()
 
         all_test += current_tests
+        count_of_test += len(current_tests)
 
         with open("data.json", 'r') as file:
             all_data = json.load(file)
-            all_data["data of tests"] = all_test
 
+        all_data["data of tests"] = all_test
+        all_data["count of elements"] = count_of_test
+            
         with open("data.json", 'w') as file:
             json.dump(all_data, file, indent=2)
                         
@@ -112,7 +118,7 @@ def initialize(stdsrc):
     return console.getMessage(is_blind=False)
 
 def generateText():
-    return "It is very simple text for test"
+    return "text for test"
 
 def test(stdsrc):
     text = generateText()
@@ -133,22 +139,27 @@ def test(stdsrc):
             num_in_text += 1
 
     end = time.time()
-
+    time_test = end - start
     console.transportToNextLine()
     count_word_in_text = len(text.split())
-    console.sendMessage(f"your spped: {int(count_word_in_text / (end - start) * 60)} your error {count_error}")
+    console.sendMessage(f"your spped: {int(count_word_in_text / time_test * 60)} your error {count_error}")
     console.transportToNextLine()
 
-    current_tests.append(Test(count_word_in_text=count_word_in_text,
+    current_tests.append(str(Test(count_word_in_text=count_word_in_text,
                               count_error=count_error,
                               speed=speed,
-                              time=time))
+                              time_test=time_test)))
+
+def end(stdsrc):
+   DadaProcessor.uploudTests()
 
 def main():
     #curses.wrapper(TestConsole)
     curses.wrapper(start)
-    while True:
+    for i in range(1):
         curses.wrapper(test)
+    curses.wrapper(end)
+
 
 if __name__ == "__main__":
     main()
